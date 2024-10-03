0db6b71681d49e89f70b2caefd2d45f7c803040b
dargasova <dianaargasova@gmail.com>
2024-10-03 15:30:38 +0300

    Initial commit

---
+++ src/main/java/ru/mysite/fbiism_store/FbiismStoreApplication.java
@@ -0,0 +1,13 @@
+package ru.mysite.fbiism_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbiismStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FbiismStoreApplication.class, args);
+	}
+
+}
